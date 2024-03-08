a16aec0ab130983867beaf54b7fe03632b59d7aa
DESKTOP-O3LMC10\User <ov13@naver.com>
2024-03-08 15:24:36 +0900

    no message

--- 240305java/src/kr/ac/kopo/day04/Task14.java
+++ 240305java/src/kr/ac/kopo/day04/Task14.java
@@ -5,13 +5,30 @@ package kr.ac.kopo.day04;
  *   ***
  *  ****
  * *****
- *  ****
- *   ***
- *    **
- *     *
+ *  ****6
+ *   ***7
+ *    **8
+ *     *9
  */
 public class Task14 {
 	public static void main(String[] args) {
-		
+		for(int i = 0; i < 9; i++) {
+			for(int j = 0; j < 5; j++) {
+				if(i < 5) {
+					if(i+j < 4) {
+						System.out.print(' ');
+					}else {
+						System.out.print('*');
+					}
+				}else {
+					if(i-j > 4) {
+						System.out.print(' ');
+					}else {
+						System.out.print('*');
+					}
+				}
+			}
+			System.out.println();
+		}
 	}
 }
--- 240305java/src/kr/ac/kopo/day04/Task16.java
+++ 240305java/src/kr/ac/kopo/day04/Task16.java
@@ -7,5 +7,21 @@ package kr.ac.kopo.day04;
  *     *     
  */
 public class Task16 {
-
+	public static void main(String[] args) {
+		for(int i = 0; i < 5; i++) {
+			for(int j = 0; j < 5; j++) {
+				if(j < i)
+					System.out.print(' ');
+				else
+					System.out.print('*');
+			}
+			for(int j = 0; j < 4; j++) {
+				if(i+j < 4)
+					System.out.print('*');
+				else
+					System.out.print(' ');
+			}
+			System.out.println();
+		}
+	}
 }
