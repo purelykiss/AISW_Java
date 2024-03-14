e230e2afd6ed201a7fcef5a8d43033ece1a6c715
DESKTOP-O3LMC10\User <ov13@naver.com>
2024-03-14 15:04:47 +0900

    no message

---
+++ 240305java/src/kr/ac/kopo/day08/MethodMain01.java
@@ -0,0 +1,28 @@
+package kr.ac.kopo.day08;
+
+/*
+  **********
+  Hello
+  **********
+  Hi
+  **********
+  Good-bye
+  **********
+ */
+
+public class MethodMain01 {
+	
+	static void printStar() {
+		System.out.println("##########");
+	}
+	
+	public static void main(String[] args) {
+		printStar();
+		System.out.println("Hello");
+		printStar();
+		System.out.println("Hi");
+		printStar();
+		System.out.println("Good-bye");
+		printStar();
+	}
+}
