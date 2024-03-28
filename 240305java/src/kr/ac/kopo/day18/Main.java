d99ff14a12b37932a4a48e15c7a8735cae9a3419
DESKTOP-O3LMC10\User <ov13@naver.com>
2024-03-28 17:36:44 +0900

    no message

--- 240305java/src/kr/ac/kopo/day18/Main.java
+++ 240305java/src/kr/ac/kopo/day18/Main.java
@@ -5,17 +5,36 @@ import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.StringTokenizer;
 
 public class Main{
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		
-		String str = br.readLine();
+		
+		
+		Set<Integer> front = new HashSet<Integer>();
+		Set<Integer> back = new HashSet<Integer>();
+		
+		for(int i = 0; i < 3; i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			int num1 = Integer.parseInt(st.nextToken());
+			int num2 = Integer.parseInt(st.nextToken());
+			
+			if (!front.add(num1)){
+				front.remove(num1);
+			}
+			if (!back.add(num2)){
+				back.remove(num2);
+			}
+		}
 		
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-
-		bw.write(Integer.toString(str.length()));
 		
+		bw.write((Integer)(front.iterator().next()) + " " +(Integer)(back.iterator().next()));
+
         bw.close();
 	}
 }
